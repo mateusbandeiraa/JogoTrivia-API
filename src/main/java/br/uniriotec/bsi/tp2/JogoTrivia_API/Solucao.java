75ad2cecd8cbe76d029f1ad173d9b0608f818509
mateusbandeiraa <mateusbandeiraa@gmail.com>
2018-10-11 23:33:03 -0300

    Initial commit

---
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Solucao.java
@@ -0,0 +1,5 @@
+package br.uniriotec.bsi.tp2.JogoTrivia_API;
+
+public abstract class Solucao {
+
+}
---
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/SolucaoOrdenada.java
@@ -0,0 +1,5 @@
+package br.uniriotec.bsi.tp2.JogoTrivia_API;
+
+public class SolucaoOrdenada extends Solucao {
+
+}
