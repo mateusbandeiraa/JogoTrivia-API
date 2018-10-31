895e68b8c2f78d97e2b639e5d9c7151f5deb35e8
mateusbandeiraa <mateusbandeiraa@gmail.com>
2018-10-31 00:58:30 -0300

    Update src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/EstadoPartida.java
    Adiciona atributo podeEncerrarPartida

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/EstadoPartida.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/EstadoPartida.java
@@ -5,14 +5,16 @@ package br.uniriotec.bsi.tp2.JogoTrivia_API;
  *
  */
 public enum EstadoPartida {
-	DISPONIVEL(true, false), EM_ANDAMENTO(false, true), ENCERRADO(false, false);
+	DISPONIVEL(true, false, false), EM_ANDAMENTO(false, true, true), ENCERRADO(false, false, false);
 	
 	final boolean aceitaNovoParticipante;
 	final boolean podeMostrarQuestao;
+	final boolean podeEncerrarPartida;
 	
-	private EstadoPartida(boolean aceitaNovoParticipante, boolean podeMostrarQuestao) {
+	private EstadoPartida(boolean aceitaNovoParticipante, boolean podeMostrarQuestao, boolean podeEncerrarPartida) {
 		this.aceitaNovoParticipante = aceitaNovoParticipante;
 		this.podeMostrarQuestao = podeMostrarQuestao;
+		this.podeEncerrarPartida = podeEncerrarPartida;
 	}
 	
 }
