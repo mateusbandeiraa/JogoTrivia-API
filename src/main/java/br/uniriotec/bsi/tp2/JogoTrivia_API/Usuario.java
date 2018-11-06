c31b78f6162fe05f55f11ca549ed0c7aeb8e091d
Rafael <37699874+Rafaelmtds@users.noreply.github.com>
2018-11-06 17:11:35 -0200

    Protótipo Usuário
    Falta JavaDoc

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Usuario.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Usuario.java
@@ -1,5 +1,53 @@
 package br.uniriotec.bsi.tp2.JogoTrivia_API;
 
+/**
+ * Representa um usuário
+ * 
+ * @author Rafael
+ *
+ */
 public class Usuario {
+	private int id;
+	private String nome;
+	private String senha;
+	private String email;
+
+	public Usuario(String nome, String senha, String email) {
+		this.nome = nome;
+		this.senha = senha;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
