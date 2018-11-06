4b7f633478a13684af39e5db266df0e94c1a9a86
Rafael <37699874+Rafaelmtds@users.noreply.github.com>
2018-11-06 17:10:47 -0200

    Protótipo Participante
    Falta JavaDoc, e implentar alguns metodos

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Participante.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Participante.java
@@ -1,5 +1,56 @@
 package br.uniriotec.bsi.tp2.JogoTrivia_API;
 
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Rafael Mota
+ *
+ */
 public class Participante {
+	private String nickname;
+	private String chave;
+	private int ajudasTempoBonusUsadas;
+	private int ajudasRemoverOpcoesUsadas;
+	private ArrayList<Interacao> interacoes;
+	private Partida partida;
+
+	public void adicionarInteracao(Interacao i) {
+		if (interacoes == null)
+			interacoes = new ArrayList<>();
+		this.interacoes.add(i);
+	}
+
+	public int pontuacaoTotal() {
+		int soma = 0;
+		for (Interacao interacao : interacoes) {
+			soma += interacao.pontuacao();
+		}
+		return soma;
+	}
+
+	public int ajudasTempoBonusDisponiveis() {
+		return (partida.getLimiteAjudasTempoBonus() - ajudasTempoBonusUsadas);
+	}
+
+	public boolean podeUsarAjudaTempoBonus() {
+		return (ajudasTempoBonusDisponiveis() < 1);
+	}
+
+	public int ajudasRemoverOpcoesDisponiveis() {
+		return (partida.getLimiteAjudasRemoverOpcoes() - ajudasRemoverOpcoesUsadas);
+	}
+
+	public boolean podeUsarAjudaRemoverOpcoes() {
+		return (ajudasRemoverOpcoesDisponiveis() < 1);
+	}
+
+	public void usarAjudaTempoBonus() {
+		// TODO
+	}
+
+	public void usarAjudaRemoverOpcoes() {
+		// TODO
+	}
 
 }
