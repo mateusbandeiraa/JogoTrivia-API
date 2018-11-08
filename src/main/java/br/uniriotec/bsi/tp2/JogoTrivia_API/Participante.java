67beb35467c3b55eb8c6fe234cf6d74530529693
Rafael <37699874+Rafaelmtds@users.noreply.github.com>
2018-11-08 15:22:16 -0200

    Adiciona Get , Set e Construtor Classe Participante
    Falta comentar o codigo

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Participante.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Participante.java
@@ -8,10 +8,22 @@ import java.util.ArrayList;
  *
  */
 public class Participante {
+	/**
+	 * Titulo que o participante escolhe
+	 */
 	private String nickname;
 	private String chave;
+	/**
+	 * A quantidade de vezes que o participante usou o bonus de Tempo extra para perguntas
+	 */
 	private int ajudasTempoBonusUsadas;
+	/**
+	 * A quantidade de vezes que o participante usou o bonus de Remover Opção para a perguntas  
+	 */
 	private int ajudasRemoverOpcoesUsadas;
+	/**
+	 * 
+	 */
 	private ArrayList<Interacao> interacoes;
 	private Partida partida;
 
@@ -52,5 +64,61 @@ public class Participante {
 	public void usarAjudaRemoverOpcoes() {
 		// TODO
 	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getChave() {
+		return chave;
+	}
+
+	public void setChave(String chave) {
+		this.chave = chave;
+	}
+
+	public int getAjudasTempoBonusUsadas() {
+		return ajudasTempoBonusUsadas;
+	}
+
+	public void setAjudasTempoBonusUsadas(int ajudasTempoBonusUsadas) {
+		this.ajudasTempoBonusUsadas = ajudasTempoBonusUsadas;
+	}
+
+	public int getAjudasRemoverOpcoesUsadas() {
+		return ajudasRemoverOpcoesUsadas;
+	}
+
+	public void setAjudasRemoverOpcoesUsadas(int ajudasRemoverOpcoesUsadas) {
+		this.ajudasRemoverOpcoesUsadas = ajudasRemoverOpcoesUsadas;
+	}
+
+	public ArrayList<Interacao> getInteracoes() {
+		return interacoes;
+	}
+
+	public void setInteracoes(ArrayList<Interacao> interacoes) {
+		this.interacoes = interacoes;
+	}
+
+	public Partida getPartida() {
+		return partida;
+	}
+
+	public void setPartida(Partida partida) {
+		this.partida = partida;
+	}
+
+	public Participante(String nickname, int ajudasTempoBonusUsadas, int ajudasRemoverOpcoesUsadas, Partida partida) {
+		this.nickname = nickname;
+		this.ajudasTempoBonusUsadas = ajudasTempoBonusUsadas;
+		this.ajudasRemoverOpcoesUsadas = ajudasRemoverOpcoesUsadas;
+		this.partida = partida;
+	}
+	
 
 }
