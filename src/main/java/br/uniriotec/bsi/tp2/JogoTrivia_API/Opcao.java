299f46ae46ae4ed196769ad13af1cd43bdd139bc
mateusbandeiraa <mateusbandeiraa@gmail.com>
2018-11-08 21:30:22 -0200

    Remove atributo final do id

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Opcao.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Opcao.java
@@ -1,46 +1,44 @@
 package br.uniriotec.bsi.tp2.JogoTrivia_API;
 
 public class Opcao {
-    private final Questao questao;
-    private final int id;
-    private String texto;
-    private final boolean estaCerto;
-    private boolean ehRemovivel;
+	private int id;
+	private String texto;
+	private final boolean estaCerto;
+	private boolean ehRemovivel;
 
-    public Opcao(Questao questao, int id, String texto, boolean estaCerto, boolean ehRemovivel) {
-        this.questao = questao;
-        this.id = id;
-        this.texto = texto;
-        this.estaCerto = estaCerto;
-        this.ehRemovivel = ehRemovivel;
-    }
+	public Opcao(String texto, boolean estaCerto, boolean ehRemovivel) {
+		this.texto = texto;
+		this.estaCerto = estaCerto;
+		this.ehRemovivel = ehRemovivel;
+	}
 
-    public Questao getQuestao() {
-        return questao;
-    }
+	public Opcao(int id, String texto, boolean estaCerto, boolean ehRemovivel) {
+		this(texto, estaCerto, ehRemovivel);
+		this.id = id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getTexto() {
-        return texto;
-    }
+	public String getTexto() {
+		return texto;
+	}
 
-    public boolean ehRemovivel() {
-        return ehRemovivel;
-    }
+	public boolean ehRemovivel() {
+		return ehRemovivel;
+	}
 
-    public void setEhRemovivel(boolean ehRemovivel) {
-        this.ehRemovivel = ehRemovivel;
-    }
-    
-    public boolean estaCerto() {
-        return estaCerto;
-    }
-    
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-    
+	public void setEhRemovivel(boolean ehRemovivel) {
+		this.ehRemovivel = ehRemovivel;
+	}
+
+	public boolean estaCerto() {
+		return estaCerto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
 }
