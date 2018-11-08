63eb4e390515fc84f4d861bca1f25ba2ff8c34a7
Rafael <37699874+Rafaelmtds@users.noreply.github.com>
2018-11-08 16:19:45 -0200

    Classe Interação Comentada e Atualizada

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Interacao.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Interacao.java
@@ -7,23 +7,41 @@ import java.util.Date;
  *
  */
 public class Interacao {
-
+	/**
+	 * Questão que está sendo exibida no momento atual. Pode ser nula.
+	 */
 	private Questao questao;
-	private Solucao solucao;
+	/**
+	 * A resposta que o Objeto Participante escolhe	
+	 */
+	private Opcao opcaoSelecionada;
+	/**
+	 * Objeto Date que representa o momento em que a questão foi respondida
+	 */
 	private Date dataCriacao;
+	/**
+	 * O objeto Partida que representa a partida atual
+	 */
 	private Partida partida;
-
+	/**
+	 * <pre>Testa a opção escolhida pelo participante e a opção correta</pre>
+	 * @return Se a questao teve exito ou nao
+	 */
 	public boolean solucaoTeveExito() {
-		return (this.solucao.equals(questao.getSolucaoOficial()));//TODO
+		return (this.opcaoSelecionada.equals(questao.getOpcaoCerta()));
 	}
-
+/**
+ * Calcula a formula da pontuação de cada resposta correta
+ * <pre>pontuacao = porcentagem do tempo restante em ralação ao tempo total</pre>
+ * @return A pontuação que o participante ganhou ao responder a questão correta0
+ */
 	public int calcularPontuacao() {
 		if (!solucaoTeveExito())
 			return 0;
 		else {
 			long tempoGasto = (dataCriacao.getTime() - 
 					partida.getDataQuestaoAtual().getTime())/1000;
-			int porcentagemUsada = (tempoGasto *100)/questao.getTempoDisponivel();//TODO 
+			int porcentagemUsada = (int)(tempoGasto *100)/questao.getTempoDisponivel();//TODO 
 			int pontuacao = 100 - porcentagemUsada;
 			return pontuacao;
 		}
