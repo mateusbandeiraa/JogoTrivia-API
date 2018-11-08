4e717e285f71ed2634eb4912cdded293af13b12d
mateusbandeiraa <mateusbandeiraa@gmail.com>
2018-11-08 21:08:09 -0200

    Novos construtures e Getters & Setters para a classe Questao

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Questao.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Questao.java
@@ -3,85 +3,109 @@ package br.uniriotec.bsi.tp2.JogoTrivia_API;
 import java.util.ArrayList;
 
 public class Questao {
-    private final int id;
-    private String textoPergunta;
-    private int tempoDisponivel; //em segundos
-    private int tempoBonus; //em segundos
-    private ArrayList<Opcao> opcoes;
-    private Opcao opcaoCerta;
-    private int quantidadeARemover;
-    
-    public Questao (int id, String textoPergunta, int tempoDisponivel, int tempoBonus) {
-        this.id = id;
-        this.textoPergunta = textoPergunta;
-        this.tempoDisponivel = tempoDisponivel;
-        this.tempoBonus = tempoBonus;
-    }
+	private int id;
+	private String textoPergunta;
+	private int tempoDisponivel; // em segundos
+	private int tempoBonus; // em segundos
+	private ArrayList<Opcao> opcoes;
+	private Opcao opcaoCerta;
+	private int quantidadeARemover;
 
-    public String getTextoPergunta() {
-        return textoPergunta;
-    }
+	public Questao(int id, String textoPergunta, int tempoDisponivel, int tempoBonus) {
+		this(textoPergunta, tempoDisponivel, tempoBonus);
+		this.id = id;
+	}
 
-    public void setTextoPergunta(String textoPergunta) {
-        this.textoPergunta = textoPergunta;
-    }
+	public Questao(String textoPergunta, int tempoDisponivel, int tempoBonus) {
+		this.textoPergunta = textoPergunta;
+		this.tempoDisponivel = tempoDisponivel;
+		this.tempoBonus = tempoBonus;
+	}
 
-    public int getTempoDisponivel() {
-        return tempoDisponivel;
-    }
+	public String getTextoPergunta() {
+		return textoPergunta;
+	}
 
-    public void setTempoDisponivel(int tempoDisponivel) {
-        this.tempoDisponivel = tempoDisponivel;
-    }
+	public void setTextoPergunta(String textoPergunta) {
+		this.textoPergunta = textoPergunta;
+	}
 
-    public int getTempoBonus() {
-        return tempoBonus;
-    }
+	public int getTempoDisponivel() {
+		return tempoDisponivel;
+	}
 
-    public void setTempoBonus(int tempoBonus) {
-        this.tempoBonus = tempoBonus;
-    }
+	public void setTempoDisponivel(int tempoDisponivel) {
+		this.tempoDisponivel = tempoDisponivel;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getTempoBonus() {
+		return tempoBonus;
+	}
 
-    public Opcao getOpcaoCerta() {
-        return opcaoCerta;
-    }
-    
-    
-    /**
-     * Recebe a base de opcoes e armazena
-     * Salva no atributo respostaCerta a opção correta
-     * @param opcoes
-     */
-    public void setOpcoes(ArrayList<Opcao> opcoes) {
-        this.opcoes = opcoes;
-        for (Opcao op : opcoes) {
-            if (op.estaCerto())
-                this.opcaoCerta = op;
-            if (op.ehRemovivel())
-                this.quantidadeARemover++;
-        }
-    }
-    
-    public double calcularTaxaDeAcerto(ArrayList<Interacao> interacoes) {
-        int qtdCertas = 0;
-        for (Interacao i : interacoes) {
-            if (i.solucaoTeveExito())
-                qtdCertas++;
-        }
-        return qtdCertas/interacoes.size()*1.0;
-    }
-    
-    public ArrayList<Opcao> removerOpcoesIncorretas () {
-        ArrayList<Opcao> opcoesARemover = new ArrayList<Opcao>();
-        for (Opcao op : opcoes) {
-            if (op.ehRemovivel())
-                opcoesARemover.add(op);
-        }
-        return opcoesARemover;
-    }
-    
+	public void setTempoBonus(int tempoBonus) {
+		this.tempoBonus = tempoBonus;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Opcao getOpcaoCerta() {
+		return opcaoCerta;
+	}
+
+	public int getQuantidadeARemover() {
+		return quantidadeARemover;
+	}
+
+	public void setQuantidadeARemover(int quantidadeARemover) {
+		this.quantidadeARemover = quantidadeARemover;
+	}
+
+	public ArrayList<Opcao> getOpcoes() {
+		return opcoes;
+	}
+
+	public void setOpcaoCerta(Opcao opcaoCerta) {
+		this.opcaoCerta = opcaoCerta;
+	}
+
+	/**
+	 * Recebe a base de opcoes e armazena
+	 * Salva no atributo respostaCerta a opção correta
+	 * 
+	 * @param opcoes
+	 */
+	public void setOpcoes(ArrayList<Opcao> opcoes) {
+		this.opcoes = opcoes;
+		for (Opcao op : opcoes) {
+			if (op.estaCerto())
+				this.opcaoCerta = op;
+			if (op.ehRemovivel())
+				this.quantidadeARemover++;
+		}
+	}
+
+	public double calcularTaxaDeAcerto(ArrayList<Interacao> interacoes) {
+		int qtdCertas = 0;
+		for (Interacao i : interacoes) {
+			if (i.solucaoTeveExito())
+				qtdCertas++;
+		}
+		return qtdCertas / interacoes.size() * 1.0;
+	}
+
+	public ArrayList<Opcao> removerOpcoesIncorretas() {
+		ArrayList<Opcao> opcoesARemover = new ArrayList<Opcao>();
+		for (Opcao op : opcoes) {
+			if (op.ehRemovivel())
+				opcoesARemover.add(op);
+		}
+		return opcoesARemover;
+	}
+
 }
