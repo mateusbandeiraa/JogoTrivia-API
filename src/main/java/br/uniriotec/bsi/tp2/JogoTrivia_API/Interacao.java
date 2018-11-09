9ef11566b506f7a8486ba33e91d5e444db846e51
Rafael <37699874+Rafaelmtds@users.noreply.github.com>
2018-11-08 22:17:16 -0200

    Classe Interação com Get, Set e Construtor.

--- src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Interacao.java
+++ src/main/java/br/uniriotec/bsi/tp2/JogoTrivia_API/Interacao.java
@@ -1,6 +1,7 @@
 package br.uniriotec.bsi.tp2.JogoTrivia_API;
 
 import java.util.Date;
+
 /**
  * 
  * @author Rafael Mota
@@ -12,7 +13,7 @@ public class Interacao {
 	 */
 	private Questao questao;
 	/**
-	 * A resposta que o Objeto Participante escolhe	
+	 * A resposta que o Objeto Participante escolhe
 	 */
 	private Opcao opcaoSelecionada;
 	/**
@@ -23,28 +24,77 @@ public class Interacao {
 	 * O objeto Partida que representa a partida atual
 	 */
 	private Partida partida;
+
 	/**
-	 * <pre>Testa a opção escolhida pelo participante e a opção correta</pre>
+	 * <pre>
+	 * Testa a opção escolhida pelo participante e a opção correta
+	 * </pre>
+	 * 
 	 * @return Se a questao teve exito ou nao
 	 */
 	public boolean solucaoTeveExito() {
 		return (this.opcaoSelecionada.equals(questao.getOpcaoCerta()));
 	}
-/**
- * Calcula a formula da pontuação de cada resposta correta
- * <pre>pontuacao = porcentagem do tempo restante em ralação ao tempo total</pre>
- * @return A pontuação que o participante ganhou ao responder a questão correta0
- */
+
+	/**
+	 * Calcula a formula da pontuação de cada resposta correta
+	 * 
+	 * <pre>
+	 * pontuacao = porcentagem do tempo restante em ralação ao tempo total
+	 * </pre>
+	 * 
+	 * @return A pontuação que o participante ganhou ao responder a questão correta0
+	 */
 	public int calcularPontuacao() {
 		if (!solucaoTeveExito())
 			return 0;
 		else {
-			long tempoGasto = (dataCriacao.getTime() - 
-					partida.getDataQuestaoAtual().getTime())/1000;
-			int porcentagemUsada = (int)(tempoGasto *100)/questao.getTempoDisponivel();//TODO 
+			long tempoGasto = (dataCriacao.getTime() - partida.getDataQuestaoAtual().getTime()) / 1000;
+			int porcentagemUsada = (int) (tempoGasto * 100) / questao.getTempoDisponivel();// TODO
 			int pontuacao = 100 - porcentagemUsada;
 			return pontuacao;
 		}
 	}
+
+	public Questao getQuestao() {
+		return questao;
+	}
+
+	public void setQuestao(Questao questao) {
+		this.questao = questao;
+	}
+
+	public Opcao getOpcaoSelecionada() {
+		return opcaoSelecionada;
+	}
+
+	public void setOpcaoSelecionada(Opcao opcaoSelecionada) {
+		this.opcaoSelecionada = opcaoSelecionada;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Partida getPartida() {
+		return partida;
+	}
+
+	public void setPartida(Partida partida) {
+		this.partida = partida;
+	}
+
+	public Interacao(Questao questao, Opcao opcaoSelecionada, Date dataCriacao, Partida partida) {
+		super();
+		this.questao = questao;
+		this.opcaoSelecionada = opcaoSelecionada;
+		this.dataCriacao = dataCriacao;
+		this.partida = partida;
+	}
+	
 
 }
